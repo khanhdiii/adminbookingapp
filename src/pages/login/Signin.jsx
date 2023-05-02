09dc6def0ce6da8efa763f3d0dce2007b82b0cde
khanhdiii <khanh12369874@gmail.com>
2023-05-03 06:42:22 +0700

    change api

---
+++ src/pages/login/Signin.jsx
@@ -0,0 +1,108 @@
+import { useContext, useState } from "react";
+import "./signin.scss";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+const SigninForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    const value = e.target.value;
+    setUsername(value);
+    if (value.length < 5) {
+      setUsernameError("Username must be at least 5 characters long");
+    } else {
+      setUsernameError("");
+    }
+  };
+
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN" });
+    try {
+      const res = await axios.post("/auth/signin", {
+        username,
+        password,
+      });
+      if (res.data.isAdmin) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+        localStorage.setItem("user", JSON.stringify(res.data.details));
+        // localStorage.setItem("access_token", res.data.access_token);
+        navigate("/");
+        window.location.reload();
+      } else {
+        dispatch({
+          type: "LOGIN_FAIL",
+          payload: { message: "You are not allowed" },
+        });
+      }
+
+      // window.location.reload();
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAIL", payload: err });
+    }
+  };
+
+  return (
+    <div className="signin-form-container">
+      <h1 className="form-title">Login Form</h1>
+      <form className="signin-form">
+        <div className="form-group">
+          <label className="label_username" htmlFor="username">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+            placeholder="Username"
+          />
+          {usernameError && (
+            <span className="error-message">{usernameError}</span>
+          )}
+
+          <label htmlFor="password" className="label_password">
+            Password
+          </label>
+          <input
+            className="input_password"
+            type="password"
+            id="password"
+            placeholder="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button
+            disabled={loading}
+            className="btn_submit"
+            type="submit"
+            onClick={handleSignin}
+          >
+            Sign In
+          </button>
+
+          <span>
+            Not have account,{" "}
+            <button onClick={() => navigate("/signup")}>create here</button>
+          </span>
+
+          {error && <span className="error">{error.message}</span>}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SigninForm;
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,9 +3,40 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import axios from "axios";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
+  const [info, setInfo] = useState({});
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleSend = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "upload");
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/dggmn6lvd/image/upload",
+        data
+      );
+
+      const { url } = uploadRes.data;
+
+      const newUser = {
+        ...info,
+        img: url,
+      };
+
+      await axios.post("/auth/signup", newUser);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(info);
 
   return (
     <div className="new">
@@ -43,10 +74,15 @@ const New = ({ inputs, title }) => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input
+                    id={input.id}
+                    onChange={handleChange}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                  />
                 </div>
               ))}
-              <button>Send</button>
+              <button onClick={handleSend}>Send</button>
             </form>
           </div>
         </div>
